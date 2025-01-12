5ff05262afe1ec310a7436e0b08481d047c2376d
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2025-01-12 10:44:03 +0700

    Menambahkan Logika Observasi State dan SnackBar pada InsertMhsView

--- app/src/main/java/com/example/session14_firebase/ui/view/InsertView.kt
+++ app/src/main/java/com/example/session14_firebase/ui/view/InsertView.kt
@@ -1,10 +1,17 @@
 package com.example.session14_firebase.ui.view
 
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.session14_firebase.PenyediaViewModel
+import com.example.session14_firebase.ui.viewmodel.FormState
 import com.example.session14_firebase.ui.viewmodel.InsertViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun InsertMhsView(
@@ -13,5 +20,30 @@ fun InsertMhsView(
     modifier: Modifier = Modifier,
     viewModel : InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ){
+    val uiState = viewModel.uiState // State utama untuk loading, success, error
+    val uiEvent = viewModel.uiEvent // State untuk form dan validasi
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+
+// Observasi perubahan state untuk snackbar dan navigasi
+    LaunchedEffect(uiState) {
+        when (uiState) {
+            is FormState.Success -> {
+                println("InsertMhsView: uiState is FormState.Success, navigate to home " + uiState.message)
+                coroutineScope.launch {
+                    snackbarHostState.showSnackbar(uiState.message) // Tampilkan snackbar
+                }
+                delay(700) // Navigasi langsung
+                onNavigate()
+                viewModel.resetSnackBarMessage() // Reset snackbar state
+            }
+            is FormState.Error -> {
+                coroutineScope.launch {
+                    snackbarHostState.showSnackbar(uiState.message)
+                }
+            }
+            else -> Unit
+        }
+    }
 
 }
