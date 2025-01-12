d917a45b2ab6fc497eaa9ec2a161eeafa6d74a3b
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2025-01-12 09:59:49 +0700

    Menambahkan data class dan fungsi MahasiswaEvent

--- app/src/main/java/com/example/session14_firebase/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/session14_firebase/ui/viewmodel/InsertViewModel.kt
@@ -1,10 +1,33 @@
 package com.example.session14_firebase.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.session14_firebase.model.Mahasiswa
 import com.example.session14_firebase.repository.MahasiswaRepository
 
 class InsertViewModel(
     private val mhs: MahasiswaRepository
 ): ViewModel(){
 
+
 }
+
+// data class Variabel yang menyimpan data input form
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
+// Menyimpan input form ke dalam entity
+fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    jenisKelamin = jenisKelamin,
+    alamat = alamat,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
