059d35387610cb63dd0ed3d4bf1530d83492c6ea
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2025-01-12 11:07:27 +0700

    Menambahkan PengelolaHalaman

---
+++ app/src/main/java/com/example/session14_firebase/ui/navigation/PengelolaHalaman.kt
@@ -0,0 +1,14 @@
+package com.example.session14_firebase.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import java.lang.reflect.Modifier
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+) {
+
+}
