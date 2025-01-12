79d3a98f7207b29bcb87a1a5c44785f5758f6873
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2025-01-12 09:48:46 +0700

    Menambahkan InsertViewModel

--- app/src/main/java/com/example/session14_firebase/dependenciesinjection/MahasiswaContainer.kt
+++ app/src/main/java/com/example/session14_firebase/dependenciesinjection/MahasiswaContainer.kt
@@ -1,7 +1,7 @@
 package com.example.session14_firebase.dependenciesinjection
 
 import com.example.session14_firebase.repository.MahasiswaRepository
-import com.example.session14_firebase.repository.NetworkMahasiswaRepository
+import com.example.session14_firebase.repository.NetworkRepositoryMhs
 import com.google.firebase.firestore.FirebaseFirestore
 
 interface AppContainer{
@@ -12,6 +12,6 @@ class MahasiswaContainer : AppContainer{
     private val firebase: FirebaseFirestore = FirebaseFirestore.getInstance()
 
     override val mahasiswaRepository: MahasiswaRepository by lazy {
-        NetworkMahasiswaRepository(firebase)
+        NetworkRepositoryMhs(firebase)
     }
 }
---
+++ app/src/main/java/com/example/session14_firebase/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.session14_firebase.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.session14_firebase.repository.MahasiswaRepository
+
+class InsertViewModel(
+    private val mhs: MahasiswaRepository
+): ViewModel(){
+
+}
