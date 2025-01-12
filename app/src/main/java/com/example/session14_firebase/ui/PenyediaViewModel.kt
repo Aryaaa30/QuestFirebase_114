99b7b446d11ddfeaf240ee42d1dbd4b214b478e0
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2025-01-12 10:30:06 +0700

    Mendaftarkan InserViewModel di PenyediaViewModel

--- app/src/main/java/com/example/session14_firebase/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/session14_firebase/ui/PenyediaViewModel.kt
@@ -1,15 +1,16 @@
 package com.example.session14_firebase
 
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.session14_firebase.ui.viewmodel.HomeViewModel
+import com.example.session14_firebase.ui.viewmodel.InsertViewModel
 
 object PenyediaViewModel{
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
+        initializer { InsertViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
     }
     fun CreationExtras.aplikasiMahasiswa(): MahasiswaApplications =
         (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as MahasiswaApplications)
